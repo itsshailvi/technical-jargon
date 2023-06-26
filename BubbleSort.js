f842a62e51b9f868e12a3e6cc696eb81966fe4b9
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-06-26 23:13:29 +0530

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,16 @@
+//Bubble sort
+
+function bubbleSort(arr) {
+    let n = arr.length
+    for (var i = 0; i < n; i++) {
+
+        for (var j = 0; j < (n - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+   return arr
+}
