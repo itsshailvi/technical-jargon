cdc7e8f579cfded3ded125fff19eb1e3abb00353
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-06-28 20:35:48 +0530

    Update CallApplyBind.js
    Call apply Bind is updated with code and basic definition to understand

--- CallApplyBind.js
+++ CallApplyBind.js
@@ -1,1 +1,33 @@
-//Call 
+//call - it invokes a function by specifying its owner object, it takes argument separately
+
+let obj1 = {
+    name: 'john',
+    subj: 122,
+}
+
+function sayHello(greet){
+    return 'hello ' + this.name +" "+greet
+}
+
+
+console.log(sayHello.call(obj1,'good Morning'))
+
+//apply- it invokes a function by specifying its owner object, it takes argument as an array
+
+let obj2 = {
+    name: 'jane',
+    car: 'maruti'
+}
+
+let obj3 = {
+    travelDetails: function(country){
+        return this.name + ' has ' + this.car+' and is going for vacation in '+ country
+    }
+}
+
+console.log(obj3.travelDetails.apply(obj2,['France']))
+
+//bind - same as call but becomes a new function 
+const greetDetails = sayHello.bind(obj1,'good afternoon')
+console.log(greetDetails())
+
