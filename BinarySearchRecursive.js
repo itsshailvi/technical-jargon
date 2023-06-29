8cefec5fef985a9e11e353898c4da1f79fc2cd16
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-06-29 18:59:37 +0530

    Update BinarySearchRecursive.js

--- BinarySearchRecursive.js
+++ BinarySearchRecursive.js
@@ -1,1 +1,12 @@
+let recursiveFunction = function (arr, x, start, end) {
+      
+    if (start > end) return false;
+    let mid=Math.floor(start+(end-start)/2);
+    if (arr[mid]===x) return true;
 
+    if(arr[mid] > x)
+        return recursiveFunction(arr, x, start, mid-1);
+    else
+ 
+        return recursiveFunction(arr, x, mid+1, end);
+}
