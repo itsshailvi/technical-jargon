d189fe4a2d778dd078bbbf7365d4d06c2524815e
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-06-30 23:20:21 +0530

    Update BinarySearchIterative.js

--- BinarySearchIterative.js
+++ BinarySearchIterative.js
@@ -1,1 +1,18 @@
+function binarySearch(arr, x)
+{   
+    let l = arr[0];
+    let r = arr.length - 1;
+    let mid;
+    while (r >= l) {
+         mid = l + Math.floor((r - l) / 2);
+        if (arr[mid] == x)
+            return mid
+        if (arr[mid] > x)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+
+    return -1;
+}
 
