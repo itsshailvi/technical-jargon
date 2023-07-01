0c6acc67fcd347ccc4a74a04012d9ff0985a3ad0
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-07-01 23:55:47 +0530

    Update StringAnagram.js

--- StringAnagram.js
+++ StringAnagram.js
@@ -1,1 +1,13 @@
+function isAnagram(one, two) {
+  //both words in small case
+  let a = one.toLowerCase();
+  let b = two.toLowerCase();
+  //check length 
+  if(a !== b)return false
+  //forming an array with each word 
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
 
