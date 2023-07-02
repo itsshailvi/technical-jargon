a844809882f7b1b96e67140ddb4016740c859dd5
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-07-02 22:49:33 +0530

    Update String_Reverse_words_in_a_given_string.js
    It will reverse a string backwords

--- String_Reverse_words_in_a_given_string.js
+++ String_Reverse_words_in_a_given_string.js
@@ -1,2 +1,12 @@
 //Reverse words in a given string
+// Input:
+// S = i.like.this.program.very.much
+// Output: much.very.program.this.like.i
 
+reverseWords(s)
+    {
+      //split('.') based on the gap if nothing then no space('') if they have space in between(' ') and then it makes them in array based on separator,
+       // reverse , reverses array like a function call join again joins it to form like a string based on parameter that acts as a separator
+ 
+        return s.split('.').reverse().join('.')
+    }
