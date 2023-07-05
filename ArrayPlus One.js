0bce470f30472fa159cb67d368b1c9dea0b497fe
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-07-05 23:58:38 +0530

    Update ArrayPlus One.js

--- ArrayPlus One.js
+++ ArrayPlus One.js
@@ -1,1 +1,19 @@
-
+ncrement(arr,n){
+    let temp = arr[0];
+if (arr[arr.length - 1] != 9) {
+  arr[arr.length - 1] += 1;
+} else {
+  for (let i = arr.length - 1; i >= 0; --i) {
+    if (arr[i] === 9) {
+      arr[i] = 0;
+    } else {
+      arr[i] += 1;
+      break;
+    }
+  }
+}
+if (arr[0] !== temp && arr[0] !== temp + 1) {
+  arr.unshift(1);
+}
+return arr
+    }
