344aa72a83c45cc8832c91ca67b58e9da40e8cd9
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-07-06 23:42:11 +0530

    Update ArrayMajority Element.js

--- ArrayMajority Element.js
+++ ArrayMajority Element.js
@@ -1,1 +1,14 @@
-
+majorityElement(a, size)
+    {
+        a = a.sort((a,b) => a-b)
+        let count = 1
+            for(let i = 0; i< size-1 ; i++){
+                if(a[i] !== a[i+1]) count = 1;
+                else{
+                    count++
+                    if(count > size/2) return a[i]
+                    
+                }
+            }
+        return (a.length === 1)? a[0] : -1
+    }
