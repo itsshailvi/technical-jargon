d8a415ec50bde8e8e94f33d4ccb8a3e94fe25007
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-07-12 22:30:00 +0530

    Update and rename NaN.js to JsNaN.js

---
+++ JsNaN.js
@@ -0,0 +1,11 @@
+// Not a number
+// typeof of NaN will return a Number
+// isNaN() function converts the given value to a Number type, and then equates to NaN
+
+
+isNaN("Hello")  // Returns true
+isNaN(345)   // Returns false
+isNaN('1')  // Returns false, since '1' is converted to Number type which results in 0 ( a number) 
+isNaN(true) // Returns false, since true converted to Number type results in 1 ( a number)
+isNaN(false) // Returns false
+isNaN(undefined) // Returns true
