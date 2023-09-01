f1e319dd8fb398470ff6f14b28bf7505f204065f
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-09-01 23:42:36 +0530

    Update Searching1.js

--- Searching1.js
+++ Searching1.js
@@ -1,1 +1,7 @@
-
+//Given a sorted and rotated array A of N distinct elements which are rotated at some point, and given an element K. The task is to find the index of the given element K in array A.
+//Input:
+//N = 9
+//A[] = {5,6,7,8,9,10,1,2,3}
+//K = 10
+//Output: 5
+//Explanation: 10 is found at index 5.
