aa756a7a07f0ab994a51608189d53bc396bd95fb
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-10-22 23:09:17 +0530

    Update onceFunction.js

--- onceFunction.js
+++ onceFunction.js
@@ -1,1 +1,15 @@
-?
+//once function
+function once(a, b) {
+  let flag = true;
+  return function () {
+    if (flag) {
+      console.log(a + b);
+      flag = false;
+    }
+  };
+}
+
+let z = once(2, 3);
+z();
+z();
+z();
