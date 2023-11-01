8e9a9940c3c002948e6911eba445652001f3f7b5
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2023-11-02 00:04:40 +0530

    Update templateLiteral.js

--- templateLiteral.js
+++ templateLiteral.js
@@ -3,3 +3,11 @@
 a) console.log("string text line 1\n" + "string text line 2");
 b) console.log(`string text line 1
 string text line 2`);
+
+2.String interpolation
+const a = 20;
+const b = 4;
+a) console.log("Twentyfour is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+b) console.log(`Twentyfour is ${a + b} andnot ${2 * a + b}.`);
+
+
