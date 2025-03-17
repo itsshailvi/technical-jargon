31e8e37cece02761bf289e632b5f82c3725ff887
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2025-03-17 23:54:35 +0530

    Update MergeSort.js

--- Sorting algos/MergeSort.js
+++ Sorting algos/MergeSort.js
@@ -1,27 +1,27 @@
 const mergeSort = (arr) => {
-    if (arr.length <= 1) return arr; // Base Case
-
-    let mid = Math.floor(arr.length / 2);
+    if(arr.length <= 1) return arr
+    // let mid = arr.slice(0,1) + Math.floor((arr[arr.length-1] - arr[0])/2)
+    let high = arr.length
+    let mid = Math.floor((high) / 2); 
+    // let mid = Math.floor( 0 + (arr.length-1 - 0)/2 )
+    // mid = Math.min(mid, arr.length-1 )
+    let left = mergeSort(arr.slice(0,mid))
+    let right = mergeSort(arr.slice(mid))
     
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-
-    return merge(left, right);
-};
+    return merge(left,right)
+}
 
 const merge = (left, right) => {
     const finalArr = []; 
-    let i = 0, j = 0;
-
-    while (i < left.length && j < right.length) {
-        if (left[i] < right[j]) {
-            finalArr.push(left[i]);
-            i++;
-        } else {
-            finalArr.push(right[j]);
-            j++;
+    let i =0, j=0
+    while(i < left.length && j < right.length){
+        if(left[i] < right[j]){
+            finalArr.push(left[i])
+            i++
+        }else{
+            finalArr.push(right[j])
+            j++
         }
     }
-
-    return [...finalArr, ...left.slice(i), ...right.slice(j)];
-};
+    
+    return [...finalArr , ...left.slice(i), ...right.slice(j)]
