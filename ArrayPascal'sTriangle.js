4d441ce742cb79280193694ab77798bef9f67666
itsshailvi <52524676+itsshailvi@users.noreply.github.com>
2025-03-18 22:49:55 +0530

    Update ArrayPascal'sTriangle.js

--- ArrayPascal'sTriangle.js
+++ ArrayPascal'sTriangle.js
@@ -1,1 +1,18 @@
+var generate = function(numRows) {
+    let res = new Array(numRows)
+    // let finalRes = [];
 
+    for(let i = 0 ; i < numRows; i++){
+        res[i] = new Array(i + 1); 
+        for(let j = 0 ; j <= i; j++){
+            if (j === 0 || i === j) {
+                res[i][j] = 1;
+            }else{
+                res[i][j] = res[i-1][j-1] + res[i-1][j]
+            }
+
+        }
+
+    }
+    return res
+};
